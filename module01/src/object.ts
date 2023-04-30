abc6b50b1f426d7b615e62aa6d8a1f6e19bc4db4
Maruf Mobin <mobinh264@gmail.com>
2023-05-01 01:34:42 +0600

    2-4: Object, Literal Types and Optional types

---
+++ module01/src/object.ts
@@ -0,0 +1,13 @@
+const users: {
+    readonly company: "programming Hero";  //  Literal Types
+    name: string;
+    age : number;
+    isMarried: boolean;
+    wife?: string;  //  Optional types
+} = {
+    company: "programming Hero",
+    name : "Rocky",
+    age : 52,
+    isMarried : true,
+    wife : "Khadija"
+};
