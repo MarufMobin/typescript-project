55b00807d7fed0375e9b1b63cabd098e210c63c3
Maruf Mobin <mobinh264@gmail.com>
2023-05-01 01:17:09 +0600

    2-3 :Array and Tuples in typescript

---
+++ module01/src/array.ts
@@ -0,0 +1,7 @@
+// Array 
+const names: string[] = [ "abul", "kabul", "babul" ];
+const rollNumbers: number[] = [34, 55, 4, 88 ];
+
+// Tupple 
+const  user: [number,string] = [ 412, 'Maruf' ]
+// key and value er khatre tupple use hoi
