f668f0cc4a978ae8875c723f2cf7e9884c2a0171
Maruf Mobin <mobinh264@gmail.com>
2023-05-16 21:31:24 +0600

    6-8: Create and Test Post Route , Refactor Code

--- module06/src/app/modules/user/user.controller.ts
+++ module06/src/app/modules/user/user.controller.ts
@@ -1,14 +1,22 @@
 import { Request, Response } from "express";
-import { createUserDB } from "./user.service";
+import { createUserDB, getUserFromDB } from "./user.service";
 
 export const createUser = async (req : Request, res : Response ) => {
-    const user = await createUserDB()
+    const data = req.body;
+    const user = await createUserDB(data)
     res.status(200).json({
         status: 'Success',
         data: user
     });
 }
-export default createUser;
+
+export const getUsers = async ( req: Request, res: Response ) =>{
+    const user = await getUserFromDB();
+    res.status(200).json({
+        status : 'Success',
+        data: user
+    })
+}
 /*  
     Pattern 
     Route -> Controller -> service 
--- module06/src/app/modules/user/user.service.ts
+++ module06/src/app/modules/user/user.service.ts
@@ -1,23 +1,13 @@
+import { IUser } from './user.interface';
 import User from './user.model';
 
-export const createUserDB = async () =>{
-    const user = await new User({
-        id: '8001',
-        role: 'student',      
-        password: 'kgf',      
-        name: { 
-            firstName: 'Mr Rocky',
-            middleName:'handsome',
-            lastName:'without frontend'
-        },   
-        dateOfBirth: '2000',
-        gender: 'male',
-        email: 'mobinh264@gmail.com',
-        contactNo: '01322409861',
-        emergencyContactNo: '01322409865',
-        presentAddress: 'Dhaka',
-        permanentAddress: "Bogura",
-    })
+export const createUserDB = async ( payload: IUser ): Promise<IUser> =>{
+    const user = await new User( payload )
     await user.save();
     return user;
+}
+
+export const getUserFromDB = async (): Promise<IUser[]> =>{
+    const users = await User.find();
+    return users;
 }
